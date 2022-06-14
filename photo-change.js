be740a6a18b47b7237d4baf7804e7bcb67366af6
juankapalacios1997 <jcmaster_97@hotmail.com>
2022-06-13 19:10:23 -0500

    last-changes

--- photo-change.js
+++ photo-change.js
@@ -1,1 +1,21 @@
-let 
+let portraitOne = document.getElementById('portrait-one');
+let portraitTwo = document.getElementById('portrait-two');
+let portraitThree = document.getElementById('portrait-three');
+let portraitFour = document.getElementById('portrait-four');
+
+portraitOne.onclick = function(){
+    portraitOne.style.display = 'none';
+    portraitTwo.style.display= 'flex';
+}
+portraitTwo.onclick = function(){
+    portraitTwo.style.display = 'none';
+    portraitThree.style.display= 'flex';
+}
+portraitThree.onclick = function(){
+    portraitThree.style.display = 'none';
+    portraitFour.style.display= 'flex';
+}
+portraitFour.onclick = function(){
+    portraitFour.style.display = 'none';
+    portraitOne.style.display= 'flex';
+}
